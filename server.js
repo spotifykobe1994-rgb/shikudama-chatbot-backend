6b7a04292406131330fde550d0fd55673723122c
spotifykobe1994-rgb <spotify.kobe.1994@gmail.com>
2026-01-24 22:53:55 +0100

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "10mb" }));
 
 // Serve per usare __dirname con ES Modules
 const __filename = fileURLToPath(import.meta.url);
